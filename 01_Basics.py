2857d0c2908d32686be35a764dc095062604845a
Zuned khan <zunedkhan107@gmail.com>
2025-07-10 14:53:11 +0530

    Add initial implementations for input handling and string operations

---
+++ 01_Basics.py
@@ -0,0 +1,16 @@
+# #Taking input and printing
+
+# name = input("name: ");
+# age = int(input("age: "));
+# price = float(input("price: "));
+
+# print("My name is " + name + " and i am " + str(age) + " years old.");
+# print("This book price is " + str(price));
+
+# # A = 12;
+# # B = 5;
+# # C = -3;
+# # print(A//B);
+# # print(A//C);
+
+# # # print("Hello World!");
---
+++ String.py
@@ -0,0 +1,73 @@
+# """String"""
+# python is allow negative indexing
+
+
+
+# a = input("a : ");
+# b = input("b : ");
+# c = input("c : ");
+# d = input("d : ");
+
+# if(a>b and a>c and a>d):
+#     print("a is largest");
+# elif(b>c and b>d):
+#     print("b is largest");
+# elif(c>d):
+#     print("c is largest")
+# else:
+#     print("d is largest")
+
+
+
+# num = int(input("Enter a number: "))
+
+# if(num%2 == 0):
+#     print("even");
+# elif(num%2 != 0):
+#     print("odd")
+
+
+
+
+# name = input("Enter your name: ");
+# marks = int(input("Enter your marks: "))
+
+# if(marks >= 90):
+#     print(name + " has grade A with " + str(marks) + " marks");
+# elif(marks >= 80 and marks < 90):
+#     print(name + " has grade B with " + str(marks) + " marks");
+# elif(marks >= 60 and marks < 80):
+#     print(name + " has grade C with " + str(marks) + " marks");
+# else:
+#     print(name + " has grade D with " + str(marks) + " marks");
+
+
+
+
+
+# # WAP to input name and ptint length
+# name = input("name: " );
+# print(name);
+# print(len(name));
+
+
+
+
+# # occurrence of a charactor
+# str = "My Name is Zuned Khan";
+# print(str.count("a"));
+
+
+
+
+# str = "Juned Khan is a Software Engineer";
+# print(str[:5]);
+# print(str[6:]);
+# print(str.endswith("eng"));
+# print(str.capitalize());
+# print(str.replace("a", "an"));
+# print(str.find("khan"));
+# print(str.count("a"));
+
+
+
