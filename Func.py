2d38e760644ad461a2e75ebcc4cde4d8cb2341af
Zuned khan <zunedkhan107@gmail.com>
2025-07-14 11:59:18 +0530

    Add initial implementations for currency conversion and loop operations

---
+++ Func.py
@@ -0,0 +1,45 @@
+# # func. defination
+
+
+# rs = input("Enter rs :")
+# def cerr_conv(rs):
+#     usd = float(rs)/83
+#     print(usd);
+
+# cerr_conv(rs)
+
+
+
+
+# cities = ["jaipur", "tonk", "dehli", "goa"]
+
+# def print_len(list):
+#     print(len(list));
+
+# def print_elem(list):
+#     for elem in list:
+#         print(elem, end=" ") # end is for print same line
+#     return list;
+
+# print_elem(cities);
+# print_len(cities);
+
+
+
+# a = int(input("a : "))
+# b = int(input("b : "))
+# c = int(input("c : "))
+
+# def calc_avg(a, b, c):
+#     print((a+b+c)/3);
+
+# calc_avg(a, b, c)
+
+
+
+
+# def cal_sum(a, b):
+#     return a + b;
+
+# result = cal_sum(1, 4);
+# print("Result: ", result)
---
+++ Loop.py
@@ -0,0 +1,140 @@
+# Break is used for stop the loop
+# continues is used for ignore any element and continue loop
+
+
+# i = 0
+# sum = 0
+# while i < 5:
+#     sum += i 
+#     i += 1
+
+# print(sum + i)
+
+
+
+
+# n = 5
+# fact = 1
+# for i in range(1, n+1):
+#     fact = i*fact
+# print(fact)
+
+ 
+
+  # pass staatement (for print empty loop)
+# for i in range(5):
+#     pass
+# print("any work without running loop")
+
+                                #    """ for loop """  
+
+# seq = range(5) # range is used for print a range of number : range(0, 3) -> output : 0, 1, 2
+# for i in seq:
+#     print(i)
+
+# for i in range(2, 10, 2): # range(start, stop, stepsize)
+#     print(i)
+
+# n = int(input("Enter any no. for table :"))
+
+# for i in range(1, 11):
+#     print(n*i)
+
+
+
+     # Linear Search
+         
+# list = [1, 3, 5, 2, 66, 44, 7, 9]
+
+# for val in list:
+#     if(val == 66):
+#         print(val)
+#         break
+#     else:
+#         print("not available")
+
+
+
+# for val in list:
+#     print(val)
+
+
+
+
+
+# str = "zunedkhan"
+
+# for char in str:
+#     if(char == 'e'):
+#         print("e found")
+#         break
+#     print(char)
+# else:
+#     print("End")
+
+# nums = [1, 2, 3, 4, 5, 6, 7] 
+
+# for val in nums:
+#     print(val)
+
+
+
+
+                                #    """ while Loop """
+
+# # qtn 6
+# i = 0
+# while i <= 5:
+#     if(i == 3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# # qtn 5
+# tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+# x = 6
+# i = 0
+# while i < len(tup):
+#     if tup[i] == x:
+#         print("inex is", i, "value is", x)
+#         break
+#     else:
+#         print("finding...")
+#     i += 1;
+# print("end of loop")
+
+
+
+
+# # qtn 4
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# i = 0
+# while i < len(list):
+#     print(list[i])
+#     i += 1
+
+
+
+# # qtn 3
+# n = 3
+# i = 1
+# while i  <= 10:
+#     print(i*n)
+#     i += 1;
+
+
+
+# # qtn 2
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+
+# # qtn 1
+# i = 1
+# while i <= 100:
+#     print(i)
+    # i += 1 
